51325d14692a0ae1e87f1a3fb7d1ee9c572039a9
adriansaracco <adriansararaco@hotmail.com>
2022-11-24 21:37:54 -0300

    mas estilos básicos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./index.css"
+
 import { NavBar } from "./Components/NavBar/NavBar";
 
 import { ItemListContainer } from "./Components/ItemListContainer/ItemListContainer";
--- src/Components/NavBar/CartWidget/CartWidget.js
+++ src/Components/NavBar/CartWidget/CartWidget.js
@@ -2,15 +2,15 @@ import "./CartWidget.css";
 
 export const CardWidget = () => {
     return (
-        <div>
+        
             <div className="">
                 <div className="conjunto-widget" >
                     <img src="./assets/Carrito.svg" width="50px" className="carrito" />
     
                    
-                    <p className="">4</p>
+                    <p className="contador">4</p>
                 </div> 
             </div>
-        </div>
+       
     )
 }
